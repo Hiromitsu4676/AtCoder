ccf0204a6462ec1332572422002728ab19afa8fd
Hiromitsu4676 <80188836+Hiromitsu4676@users.noreply.github.com>
2021-04-14 00:03:35 +0900

    Update acbs_daydream_rec.py

--- acbs_daydream_rec.py
+++ acbs_daydream_rec.py
@@ -1,8 +1,3 @@
-'''
-未完成
-'''
-
-
 S=input()
 
 def rec(s,w):
@@ -10,10 +5,12 @@ def rec(s,w):
         return True
 
     if len(w)==5:
-        if s[:5]=='dream' or 'erase':
+        if s[:5]=='dream':
+            s=s[5:]
+        elif s[:5]=='erase':
             s=s[5:]
         else:
-            return False
+            return False    
     if len(w)==6:
         if s[:6]=='eraser':
             s=s[6:]
@@ -31,4 +28,7 @@ def rec(s,w):
 
 ans=rec(S,'')
 
-print(ans)
+if ans==True:
+    print('YES')
+else:
+    print('NO')
