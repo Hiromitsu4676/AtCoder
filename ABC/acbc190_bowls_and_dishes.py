700b0a3ecfc47237e597443e514920162a1500e9
Hiromitsu4676 <80188836+Hiromitsu4676@users.noreply.github.com>
2021-05-11 23:57:41 +0900

    Update acbc190_bowls_and_dishes.py

--- ABC/acbc190_bowls_and_dishes.py
+++ ABC/acbc190_bowls_and_dishes.py
@@ -1,70 +1,24 @@
+import itertools
+
 N,M=map(int,input().split())
-M_list=[[] for _ in range(M)]
+condi=[[] for _ in range(M)]
 for i in range(M):
     temp=list(map(int,input().split()))
-    M_list[i]=temp
+    condi[i]=temp
 K=int(input())
-P_list=[[] for _ in range(K)]
+choice=[[] for _ in range(K)]
 for j in range(K):
     temp=list(map(int,input().split()))
-    P_list[j]=temp
+    choice[j]=temp
 
-print(M_list)
-print(P_list)
+rslt=0
 
-#標準入力
-n, m = map(int,input().split())
+for balls in itertools.product(*choice):
+    count=0
+    for a,b in condi:
+        if a in balls and b in balls:
+            count+=1
+    if count>rslt:
+        rslt=count
 
-A = []
-B = []
-for i in range(m):
-    a, b = map(int,input().split())
-    A.append(a)
-    B.append(b)
-
-k = int(input())
-
-C = []
-D = []
-for i in range(k):
-    c, d = map(int,input().split())
-    C.append(c)
-    D.append(d)
-
-# 満たした条件
-ans = 0
-
-#bit全探索(2^k回繰り返す)
-#人iがCの皿をおいたかDの皿をおいたかを二進数表記にする
-#0101ならCDCDを選んだ、と考える
-
-
-for num in range(2 ** k):
-    
-    dish = [0] * n
-    #皿の上のボールの数を格納するリスト。初期はすべてゼロ
-
-    for j in range(k):
-        '''
-        numがのとき二進数表記で0101
-        0回シフトすると：0101
-        1回シフトすると：0010
-        2回シフトすると：0001
-        '''
-        bit=bin(num)
-        if (num >> j) & 1:
-            #J番目が1ならばDのボールをおく
-            dish[D[j]-1] += 1
-        else:
-            #J番目が0ならばCのボールをおく
-            dish[C[j]-1] += 1
-
-    sum_i = 0
-    for l in range(m):
-        if dish[A[l]-1] > 0 and dish[B[l]-1] > 0:
-            sum_i += 1
-
-    if ans < sum_i:
-        ans = sum_i
-else:
-    print(ans)
+print(rslt)
